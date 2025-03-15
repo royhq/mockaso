6105ae06192136c5c9ef63f054b4acd7536843c9
royhq <royca.dev@gmail.com>
2025-03-15 17:32:56 -0300

    Added stub object.
    Represent the request to match and the response to write.

---
+++ stub.go
@@ -0,0 +1,66 @@
+package mockaso
+
+import (
+	"net/http"
+)
+
+type (
+	StubMatcherRule  func() requestMatcherFunc
+	StubResponseRule func(*stubResponse)
+)
+
+type Stub interface {
+	StubResponder
+	Match(...StubMatcherRule) StubResponder
+}
+
+type StubResponder interface {
+	Respond(...StubResponseRule)
+}
+
+type stub struct {
+	matchers []requestMatcherFunc
+	response *stubResponse
+}
+
+func (s *stub) Match(rules ...StubMatcherRule) StubResponder {
+	for _, rule := range rules {
+		s.matchers = append(s.matchers, rule())
+	}
+
+	return s
+}
+
+func (s *stub) Respond(rules ...StubResponseRule) {
+	for _, rule := range rules {
+		rule(s.response)
+	}
+}
+
+func (s *stub) match(r *http.Request) bool {
+	for _, match := range s.matchers {
+		if !match(s, r) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (s *stub) write(w http.ResponseWriter) {
+	w.WriteHeader(s.response.statusCode)
+	_, _ = w.Write(s.response.body)
+}
+
+type stubResponse struct {
+	statusCode int
+	body       []byte
+	headers    map[string]string
+}
+
+func newStubResponse() *stubResponse {
+	return &stubResponse{
+		statusCode: http.StatusOK,
+		headers:    make(map[string]string),
+	}
+}
