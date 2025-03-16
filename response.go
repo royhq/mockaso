061ca07e3abfb2db6f028fef336f1cf6c7627bca
royhq <royca.dev@gmail.com>
2025-03-16 15:04:29 -0300

    Added WithBody rule

--- response.go
+++ response.go
@@ -1,9 +1,43 @@
 package mockaso
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+)
+
 type StubResponseRule func(*stubResponse)
 
+// WithStatusCode sets the response status code.
 func WithStatusCode(statusCode int) StubResponseRule {
 	return func(r *stubResponse) {
 		r.statusCode = statusCode
+	}
+}
+
+// WithBody sets the response body.
+func WithBody(body any) StubResponseRule {
+	data, err := anyBodyToBytes(body)
+	if err != nil {
+		panic(fmt.Errorf("WithBody err: failed to read body: %w", err))
+	}
+
+	return func(r *stubResponse) {
+		r.body = data
+	}
+}
+
+func anyBodyToBytes(body any) ([]byte, error) {
+	switch v := body.(type) {
+	case []byte:
+		return v, nil
+	case string:
+		return []byte(v), nil
+	case json.RawMessage:
+		return v, nil
+	case io.Reader:
+		return io.ReadAll(v)
+	default:
+		return []byte(fmt.Sprintf("%v", v)), nil
 	}
 }
--- response_test.go
+++ response_test.go
@@ -1,8 +1,11 @@
 package mockaso_test
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -48,4 +51,84 @@ func TestWithStatusCode(t *testing.T) {
 
 		assert.Equal(t, http.StatusOK, httpResp.StatusCode)
 	})
+}
+
+func TestWithBody(t *testing.T) {
+	t.Parallel()
+
+	server := mockaso.MustStartNewServer(mockaso.WithLogger(t))
+	t.Cleanup(server.MustShutdown)
+
+	t.Run("should return the specified body", func(t *testing.T) {
+		testCases := map[string]struct {
+			url          string
+			body         any
+			expectedBody string
+		}{
+			"bytes body": {
+				url:          "/test/bytes",
+				body:         []byte("test bytes body"),
+				expectedBody: `test bytes body`,
+			},
+			"string body": {
+				url:          "/test/string",
+				body:         "test string body",
+				expectedBody: `test string body`,
+			},
+			"int body": {
+				url:          "/test/int",
+				body:         123,
+				expectedBody: `123`,
+			},
+			"json raw body": {
+				url:          "/test/json",
+				body:         json.RawMessage(`{"name":"john"}`),
+				expectedBody: `{"name":"john"}`,
+			},
+			"string reader body": {
+				url:          "/test/string-reader",
+				body:         strings.NewReader("string reader body"),
+				expectedBody: `string reader body`,
+			},
+			"buffer body": {
+				url:          "/test/buffer",
+				body:         bytes.NewBuffer([]byte("buffer body")),
+				expectedBody: `buffer body`,
+			},
+			"map body": {
+				url:          "/test/map",
+				body:         map[string]any{"name": "john", "age": 57},
+				expectedBody: `map[age:57 name:john]`,
+			},
+			"struct body": {
+				url:          "/test/struct",
+				body:         userResponse{Name: "john", Age: 57},
+				expectedBody: `{john 57}`,
+			},
+		}
+
+		for name, tc := range testCases {
+			t.Run(name, func(t *testing.T) {
+				t.Parallel()
+
+				server.Stub(http.MethodGet, mockaso.URL(tc.url)).
+					Respond(
+						mockaso.WithStatusCode(http.StatusOK),
+						mockaso.WithBody(tc.body),
+					)
+
+				httpReq, _ := http.NewRequest(http.MethodGet, tc.url, http.NoBody)
+				httpResp, err := server.Client().Do(httpReq)
+				require.NoError(t, err)
+
+				assert.Equal(t, http.StatusOK, httpResp.StatusCode)
+				assertBodyString(t, tc.expectedBody, httpResp)
+			})
+		}
+	})
+}
+
+type userResponse struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
 }
